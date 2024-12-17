b0cb6d47537208e242bac90cf0e2db4a4711e643
xclovehsy <2505327613@qq.com>
2024-12-17 13:29:00 +0800

    add model2d

--- model.py
+++ model.py
@@ -38,6 +38,58 @@ class Model1(nn.Module):
         x = self.relu(x)
         x = self.fc2(x)
         return x
+    
+class Model2d(nn.Module):
+    def __init__(self, input_dim):
+        super(Model2d, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 8)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(8, 1)
+
+    def forward(self, x):
+        b_total = x[:, -3:].sum(dim=1).unsqueeze(1)
+        b_total = self._bpf(b_total)
+    
+        A = self._cal_matrix_A(x[:, -3:])
+        A = self._bpf(A)
+
+        beta_tl = b_total / A
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        output = (x + beta_tl) * A
+        return output.sum(dim=1)
+    
+    def _bpf(self, x):
+        return x
+    
+    def _cal_matrix_A(self, B):
+        B_magnitude = torch.norm(B, dim=1, keepdim=True)
+        B_hat = B / B_magnitude
+        Bx_hat, By_hat, Bz_hat = B_hat[:, 0], B_hat[:, 1], B_hat[:, 2]
+        sym_terms = torch.stack([
+            B_magnitude.squeeze() * Bx_hat * Bx_hat,
+            B_magnitude.squeeze() * Bx_hat * By_hat,
+            B_magnitude.squeeze() * Bx_hat * Bz_hat,
+            B_magnitude.squeeze() * By_hat * By_hat,
+            B_magnitude.squeeze() * By_hat * Bz_hat,
+            B_magnitude.squeeze() * Bz_hat * Bz_hat,
+        ], dim=1)
+        full_terms = torch.stack([
+            B_magnitude.squeeze() * Bx_hat * Bx_hat,
+            B_magnitude.squeeze() * Bx_hat * By_hat,
+            B_magnitude.squeeze() * Bx_hat * Bz_hat,
+            B_magnitude.squeeze() * By_hat * Bx_hat,
+            B_magnitude.squeeze() * By_hat * By_hat,
+            B_magnitude.squeeze() * By_hat * Bz_hat,
+            B_magnitude.squeeze() * Bz_hat * Bx_hat,
+            B_magnitude.squeeze() * Bz_hat * By_hat,
+            B_magnitude.squeeze() * Bz_hat * Bz_hat,
+        ], dim=1)
+        delta = torch.cat([B_hat, sym_terms, full_terms], dim=1)
+        return delta
 
 
 if __name__ == '__main__':
@@ -60,10 +112,11 @@ if __name__ == '__main__':
                                     df_options[df_options['line'] == line]['t_end'].values[0]])  # get Boolean indices
     train_ind = ~test_ind
     exclude_features = ['line', 'flight', 'year', 'doy', 'tt', 'drape', 'mag_1_c', 'mag_1_lag', 'mag_1_dc', 'mag_1_igrf',
-                        'mag_1_uc', 'ogs_mag', 'ogs_alt', 'radar', 'topo']
+                        'mag_1_uc', 'ogs_mag', 'ogs_alt', 'radar', 'topo', 'flux_b_x', 'flux_b_y', 'flux_b_z']
     fields20 = "./datasets/fields_sgl_2020.csv"
     features = pd.read_csv(fields20, header=None).squeeze("columns").astype(str).tolist()
-    features = [item for item in features if item not in exclude_features]
+    features = [item for item in features if item not in exclude_features] + ['flux_b_x', 'flux_b_y', 'flux_b_z']
+    print(features)
     dataset_train = MagCompDataset(xyz, train_ind, features)
     print("train num:", dataset_train.__len__())
     dataset_val = MagCompDataset(xyz, test_ind, features)
@@ -71,9 +124,12 @@ if __name__ == '__main__':
     dataloader_train = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
     dataloader_val = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False, num_workers=0)
 
+    input_dim = dataset_train.x.shape[1]
     model_type = 'Model1'
-    print("input dim:", dataset_train.x.shape[1])
-    model = Model1(dataset_train.x.shape[1]).to(device)
+    print("input dim:", input_dim)
+    # model = Model1(dataset_train.x.shape[1]).to(device)
+    model = Model2d(input_dim).to(device)
+
     criterion = nn.MSELoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
